f5fbf4e24c742b8aec020073b9958aa87f81fb6e
Mugamba Bruno <brunohectre@gmail.com>
2024-08-16 07:54:53 +0300

    modified

--- src/app/contact/components/ContactForm.tsx
+++ src/app/contact/components/ContactForm.tsx
@@ -1,32 +1,89 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { motion } from 'framer-motion';
+import { useForm, ValidationError } from '@formspree/react';
+import ReactCanvasConfetti from 'react-canvas-confetti';
+import type { IConfettiOptions } from 'react-canvas-confetti';
 
-const ContactForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
-    const [formState, setFormState] = useState('idle'); // 'idle', 'error', or 'success'
+type ConfettiConfig = {
+    spread: number;
+    startVelocity?: number;
+    decay?: number;
+    scalar?: number;
+};
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (name && email && subject && message) {
-            setFormState('success');
-            console.log({ name, email, subject, message });
-        } else {
-            setFormState('error');
-        }
+const canvasStyles: React.CSSProperties = {
+    position: 'fixed',
+    pointerEvents: 'none',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0
+};
+
+const ContactForm: React.FC = () => {
+    const [state, handleSubmit] = useForm("xblrkgwy");
+    const [isConfettiActive, setIsConfettiActive] = useState<boolean>(false);
+    const confettiRef = useRef<ReactCanvasConfetti | null>(null);
+
+    const getInstance = useCallback((instance: ReactCanvasConfetti | null) => {
+        confettiRef.current = instance;
+    }, []);
+
+    const onSubmitSuccess = () => {
+        setIsConfettiActive(true);
+        setTimeout(() => setIsConfettiActive(false), 5000); // Stop confetti after 5 seconds
     };
 
-    const inputClasses = (fieldValue: string) => `
-        mt-1 block w-full rounded-lg
-        border-2 ${formState === 'error' && !fieldValue ? 'border-red-300' : formState === 'success' ? 'border-teal-300' : 'border-gray-300'} 
-        focus:border-orange-500 focus:ring-2 focus:ring-orange-200 focus:ring-opacity-50
-        transition duration-300 ease-in-out
-        py-3 px-4 text-gray-700 text-base
-        bg-white bg-opacity-90 hover:bg-opacity-100
-    `;
+    const makeShot = useCallback((particleRatio: number, opts: ConfettiConfig) => {
+        confettiRef.current?.({
+            ...opts,
+            origin: { y: 0.7 },
+            particleCount: Math.floor(200 * particleRatio),
+        });
+    }, []);
+
+    const fire = useCallback(() => {
+        makeShot(0.25, {
+            spread: 26,
+            startVelocity: 55,
+        });
+
+        makeShot(0.2, {
+            spread: 60,
+        });
+
+        makeShot(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8,
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2,
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 45,
+        });
+    }, [makeShot]);
+
+    React.useEffect(() => {
+        if (isConfettiActive) {
+            fire();
+        }
+    }, [isConfettiActive, fire]);
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        await handleSubmit(e);
+        if (state.succeeded) {
+            onSubmitSuccess();
+        }
+    };
 
     return (
         <div className="min-h-[10vh] bg-gradient-to-br from-orange-100 via-orange-50 to-white flex items-center justify-center my-36 ">
@@ -58,77 +115,79 @@ const ContactForm = () => {
                         </div>
                     </motion.div>
 
-                    <motion.form
-                        initial={{ opacity: 0, x: 50 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.5, delay: 0.4 }}
-                        onSubmit={handleSubmit}
-                        className="p-10 md:w-2/3 space-y-8"
-                    >
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    {state.succeeded ? (
+                        <div className="p-10 md:w-2/3 flex items-center justify-center">
+                            <p className="text-2xl font-semibold text-green-600">Thanks for joining!</p>
+                        </div>
+                    ) : (
+                        <motion.form
+                            initial={{ opacity: 0, x: 50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.5, delay: 0.4 }}
+                            onSubmit={onSubmit}
+                            className="p-10 md:w-2/3 space-y-8"
+                        >
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                                <div>
+                                    <label htmlFor="name" className="block text-sm font-medium text-gray-700 mb-2">Your Name</label>
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        name='name'
+                                        className="mt-1 block w-full rounded-lg border-2 border-gray-300 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 focus:ring-opacity-50 transition duration-300 ease-in-out py-3 px-4 text-gray-700 text-base bg-white bg-opacity-90 hover:bg-opacity-100"
+                                        required
+                                    />
+                                    <ValidationError prefix="Name" field="name" errors={state.errors} />
+                                </div>
+                                <div>
+                                    <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">Your Email</label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        name='email'
+                                        className="mt-1 block w-full rounded-lg border-2 border-gray-300 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 focus:ring-opacity-50 transition duration-300 ease-in-out py-3 px-4 text-gray-700 text-base bg-white bg-opacity-90 hover:bg-opacity-100"
+                                        required
+                                    />
+                                    <ValidationError prefix="Email" field="email" errors={state.errors} />
+                                </div>
+                            </div>
                             <div>
-                                <label htmlFor="name" className="block text-sm font-medium text-gray-700 mb-2">Your Name</label>
+                                <label htmlFor="subject" className="block text-sm font-medium text-gray-700 mb-2">Your Subject</label>
                                 <input
                                     type="text"
-                                    id="name"
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                    className={inputClasses(name)}
+                                    id="subject"
+                                    name='subject'
+                                    className="mt-1 block w-full rounded-lg border-2 border-gray-300 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 focus:ring-opacity-50 transition duration-300 ease-in-out py-3 px-4 text-gray-700 text-base bg-white bg-opacity-90 hover:bg-opacity-100"
                                     required
                                 />
+                                <ValidationError prefix="Subject" field="subject" errors={state.errors} />
                             </div>
                             <div>
-                                <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">Your Email</label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    className={inputClasses(email)}
+                                <label htmlFor="message" className="block text-sm font-medium text-gray-700 mb-2">Message</label>
+                                <textarea
+                                    id="message"
+                                    rows={3}
+                                    name='message'
+                                    className="mt-1 block w-full rounded-lg border-2 border-gray-300 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 focus:ring-opacity-50 transition duration-300 ease-in-out py-3 px-4 text-gray-700 text-base bg-white bg-opacity-90 hover:bg-opacity-100"
+                                    placeholder="Write your message here..."
                                     required
-                                />
+                                ></textarea>
+                                <ValidationError prefix="Message" field="message" errors={state.errors} />
                             </div>
-                        </div>
-                        <div>
-                            <label htmlFor="subject" className="block text-sm font-medium text-gray-700 mb-2">Your Subject</label>
-                            <input
-                                type="text"
-                                id="subject"
-                                value={subject}
-                                onChange={(e) => setSubject(e.target.value)}
-                                className={inputClasses(subject)}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="message" className="block text-sm font-medium text-gray-700 mb-2">Message</label>
-                            <textarea
-                                id="message"
-                                rows={3}
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                className={inputClasses(message)}
-                                placeholder="Write your message here..."
-                                required
-                            ></textarea>
-                        </div>
-                        <motion.button
-                            whileHover={{ scale: 1.02 }}
-                            whileTap={{ scale: 0.98 }}
-                            type="submit"
-                            className="w-full bg-orange-500 text-white py-4 px-6 rounded-lg text-lg font-semibold hover:bg-orange-600 transition duration-300 ease-in-out"
-                        >
-                            Send Message
-                        </motion.button>
-                        {formState === 'error' && (
-                            <p className="text-red-500 text-sm mt-2">Please fill in all fields correctly.</p>
-                        )}
-                        {formState === 'success' && (
-                            <p className="text-teal-500 text-sm mt-2">Message sent successfully!</p>
-                        )}
-                    </motion.form>
+                            <motion.button
+                                whileHover={{ scale: 1.02 }}
+                                whileTap={{ scale: 0.98 }}
+                                type="submit"
+                                disabled={state.submitting}
+                                className="w-full bg-orange-500 text-white py-4 px-6 rounded-lg text-lg font-semibold hover:bg-orange-600 transition duration-300 ease-in-out"
+                            >
+                                Send Message
+                            </motion.button>
+                        </motion.form>
+                    )}
                 </div>
             </motion.div>
+            <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
         </div>
     );
 };
